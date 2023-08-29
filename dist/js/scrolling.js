807030c163055968110bb973b759f2ed62ace974
MacMittenss <scttjssy@gmail.com>
2023-08-29 19:56:02 -0400

    first commit

---
+++ dist/js/scrolling.js
@@ -0,0 +1,24 @@
+// Scrolling Javascrript 
+const items = document.querySelectorAll('.process-div');
+
+const isInViewport = el => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight * 5 / 4.5 || document.documentElement.clientHeight) &&
+    rect.right >= 0
+  );
+};
+
+const run = () =>
+  items.forEach(item => {
+    if (isInViewport(item)) {
+      item.classList.add('show');
+    }
+  });
+
+// Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
